872db7bdd7ebecc069b7d73b5b5d369e97822bc6
dondoco7 <dondoco7@gmail.com>
2015-10-29 23:01:29 +0900

    test rgb2hsl

--- spec/lib/vibrant_spec.rb
+++ spec/lib/vibrant_spec.rb
@@ -15,10 +15,19 @@ describe Vibrant::Vibrant do
   
   describe 'rgb2hsl' do 
     it 'white' do 
-      expect(Vibrant::Vibrant.rgb2hsl(255,255,255)).to eq [0,0,100]
+      expect(Vibrant::Vibrant.rgb2hsl(255,255,255)).to eq [0,0,1.0]
+    end
+    it 'black' do 
+      expect(Vibrant::Vibrant.rgb2hsl(0,0,0)).to eq [0,0,0]
     end
     it 'red' do 
-      expect(Vibrant::Vibrant.rgb2hsl(255,0,0)).to eq [0,0,100]
+      expect(Vibrant::Vibrant.rgb2hsl(255,0,0)).to eq [0.0,1.0, 0.5]
+    end
+    it 'green' do 
+      expect(Vibrant::Vibrant.rgb2hsl(0,255,0)).to eq [120.0/360.0,1.0,0.5]
+    end
+    it 'blue' do 
+      expect(Vibrant::Vibrant.rgb2hsl(0,0,255)).to eq [240.0/360.0,1.0,0.5]
     end
   end
   
